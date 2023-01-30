cc80ef082306708f6486169ea9bbb77dab7863ba
rahul <rsnytl@gmail.com>
2023-01-30 18:09:23 +0530

    successful run initial setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Hello from './Hello';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<Hello />);
-// let helloDiv = document.createElement('div');
-// helloDiv.innerHTML = 'Hello from Javascript!';
-// document.body.append(helloDiv);
-
-// const sayHelloManyTimes = times =>
-//   new Array(times).fill(1).map((_, i) => `Hello ${i + 1}`);
-
-// helloDiv = document.createElement('div');
-// helloDiv.innerHTML = sayHelloManyTimes(10).join('<br/>');
-// document.body.append(helloDiv);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,69 @@
-const path = require('path');
+const webpack = require('webpack');
+// const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
-module.exports = {
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+const config = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'build')
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    clean: true,
+    publicPath: '/'
   },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'build')
-    },
-    port: 3000
-  },
+  mode: isDevelopment ? 'development' : 'production',
+
   module: {
-    // exclude node_modules
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.[jt]sx?$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        loader: 'babel-loader',
+        options: {
+          plugins: ['react-refresh/babel']
+        }
+      },
+      // {
+      //   test: /\.(js|jsx)$/,
+      //   use: 'babel-loader',
+      //   exclude: /node_modules/
+      // },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(js|jsx)$/, // <-- added `|jsx` here
-        exclude: /node_modules/,
-        use: ['babel-loader']
+        test: /\.(jpg|png|jpeg)$/,
+        use: {
+          loader: 'url-loader'
+        }
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack', 'url-loader']
       }
     ]
   },
-  // pass all js files through Babel
-  resolve: {
-    extensions: ['*', '.js', '.jsx'] // <-- added `.jsx` here
+  devServer: {
+    historyApiFallback: true,
+    client: { overlay: false },
+    static: {
+      directory: './public'
+    }
   },
   plugins: [
+    isDevelopment && new ReactRefreshPlugin(),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html')
+      filename: './index.html',
+      template: './public/index.html'
     })
-  ],
-  mode: 'development'
+  ].filter(Boolean),
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 };
+
+module.exports = config;
